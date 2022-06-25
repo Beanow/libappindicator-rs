753243fec8c3dd2c714c861f41d97abc22635293
Beanow <497556+Beanow@users.noreply.github.com>
2022-06-25 11:57:49 +0200

    Use ENV to configure example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,16 +1,33 @@
-use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use gtk::prelude::*;
 use libappindicator::{AppIndicator, AppIndicatorStatus};
 
 fn main() {
   gtk::init().unwrap();
+
   let mut indicator = AppIndicator::new("libappindicator test application", "");
   indicator.set_status(AppIndicatorStatus::Active);
-  let icon_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples");
-  indicator.set_icon_theme_path(icon_path.to_str().unwrap());
-  indicator.set_icon_full("rust-logo", "icon");
+
+  let icon_path: Option<PathBuf> = if let Some(dir) = option_env!("TRAY_ICON_DIR") {
+    match dir {
+      "" => None,
+      dir => Some(Path::new(dir).to_path_buf()),
+    }
+  } else if let Some(dir) = option_env!("CARGO_MANIFEST_DIR") {
+    Some(Path::new(dir).join("examples"))
+  } else {
+    None
+  };
+
+  if let Some(icon_path) = icon_path {
+    indicator.set_icon_theme_path(icon_path.to_str().unwrap());
+  }
+
+  let icon_name = option_env!("TRAY_ICON_NAME").unwrap_or("rust-logo");
+  let icon_desc = option_env!("TRAY_ICON_DESC").unwrap_or("icon");
+  indicator.set_icon_full(icon_name, icon_desc);
+
   let mut m = gtk::Menu::new();
   let mi = gtk::CheckMenuItem::with_label("Hello Rust!");
   mi.connect_activate(|_| {
@@ -19,5 +36,6 @@ fn main() {
   m.append(&mi);
   indicator.set_menu(&mut m);
   m.show_all();
+
   gtk::main();
 }
