86c92f217ac41f85dbf8f5607a39eb92ef9e5d5b
Beanow <497556+Beanow@users.noreply.github.com>
2022-06-25 12:27:17 +0200

    Vars should be runtime, not compile time

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,4 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::{
+  env,
+  path::{Path, PathBuf},
+};
 
 use gtk::prelude::*;
 use libappindicator::{AppIndicator, AppIndicatorStatus};
@@ -9,8 +12,8 @@ fn main() {
   let mut indicator = AppIndicator::new("libappindicator test application", "");
   indicator.set_status(AppIndicatorStatus::Active);
 
-  let icon_path: Option<PathBuf> = if let Some(dir) = option_env!("TRAY_ICON_DIR") {
-    match dir {
+  let icon_path: Option<PathBuf> = if let Ok(dir) = env::var("TRAY_ICON_DIR") {
+    match dir.as_str() {
       "" => None,
       dir => Some(Path::new(dir).to_path_buf()),
     }
@@ -25,10 +28,10 @@ fn main() {
     indicator.set_icon_theme_path(icon_path.to_str().unwrap());
   }
 
-  let icon_name = option_env!("TRAY_ICON_NAME").unwrap_or("rust-logo");
-  let icon_desc = option_env!("TRAY_ICON_DESC").unwrap_or("icon");
+  let icon_name = env::var("TRAY_ICON_NAME").unwrap_or("rust-logo".to_string());
+  let icon_desc = env::var("TRAY_ICON_DESC").unwrap_or("icon".to_string());
   println!("set_icon_full: {:?} {:?}", icon_name, icon_desc);
-  indicator.set_icon_full(icon_name, icon_desc);
+  indicator.set_icon_full(&icon_name, &icon_desc);
 
   let mut m = gtk::Menu::new();
   let mi = gtk::CheckMenuItem::with_label("Hello Rust!");
